0ed8b223f981e19740c4eb6eb5be211168156449
kuangwanghangping <142001244+kuangwanghangping@users.noreply.github.com>
2024-06-19 20:04:25 +0800

    Update df_excel.py

--- df_excel.py
+++ df_excel.py
@@ -1,3 +1,4 @@
+###########这个是将df保存成excel格式
 df = pro.cb_basic(fields="ts_code,bond_short_name,stk_code,stk_short_name,list_date,delist_date")
 excel_file = 'bond_data.xlsx'
 df.to_excel(excel_file, index=False, startrow=1)#startrow=1的意思是第一行空着
