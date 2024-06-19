0e3a61b90b8394ae08820b76c93f8c82c5400c3f
kuangwanghangping <142001244+kuangwanghangping@users.noreply.github.com>
2024-06-19 20:01:51 +0800

    Add files via upload

---
+++ df_excel.py
@@ -0,0 +1,4 @@
+df = pro.cb_basic(fields="ts_code,bond_short_name,stk_code,stk_short_name,list_date,delist_date")
+excel_file = 'bond_data.xlsx'
+df.to_excel(excel_file, index=False, startrow=1)#startrow=1的意思是第一行空着
+print(f"数据已保存到 {excel_file} 文件中。")
